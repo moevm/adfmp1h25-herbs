a680247a1a26cd26e29d1059ca55443af17d1897
corvussharp <gerasimenkoooo37@gmail.com>
2025-03-10 19:17:10 +0300

    theme

---
+++ app/src/main/java/com/herbsapp/presentation/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.herbsapp.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primary = Color(0xFFDE7254)
+val white = Color(0xFFFAF9F9)
+val button = Color(0xFFD0D0D0).copy(alpha = 0.95f)
+val border = Color(0xFFE7E7EF)
+val black = Color(0xFF101018)
+val gray = Color(0xFF828F9C)
+val error = Color(0xFFF44336).copy(alpha = 0.8f)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
