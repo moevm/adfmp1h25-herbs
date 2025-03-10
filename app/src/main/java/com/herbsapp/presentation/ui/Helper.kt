a50161500f9e9097b11a8df452c3ad7e8d63ca36
corvussharp <gerasimenkoooo37@gmail.com>
2025-03-10 19:18:35 +0300

    add helper/navigation

---
+++ app/src/main/java/com/herbsapp/presentation/ui/Helper.kt
@@ -0,0 +1,35 @@
+package com.herbsapp.presentation.ui
+
+import android.content.Context
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import com.herbsapp.data.room.entity.ElementEntity
+
+fun Context.imageLoader() = ImageLoader.Builder(this).components{ SvgDecoder.Factory()}.build()
+
+data class Sign(
+    val title: String,
+    val icon: Int,
+    val isChoose: Boolean,
+    var selectedElement: ElementEntity? = null,
+)
+
+data class Determiner(
+    val title: String,
+    val variants: List<String>,
+    var selectVariant: String? = null
+)
+
+sealed class SignValue {
+    object All: SignValue()
+    object Class: SignValue()
+    object Genus: SignValue()
+    object Taste: SignValue()
+    object Family: SignValue()
+}
+
+sealed class Resource<out R> {
+    data class Success<out R>(val result: R) : Resource<R>()
+    data class Failure(val exception: Exception) : Resource<Nothing>()
+    object Loading : Resource<Nothing>()
+}
